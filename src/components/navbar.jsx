8dacd7abcb549e9b66534bae552b18b9721f2587
PhantomGrin <savindi.wijenayaka@gmail.com>
2020-03-30 14:02:32 +0530

    updates

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-        <nav className="navbar navbar-dark bg-dark">
-          <span className="navbar-brand mb-0 h1">Navbar</span>
-        </nav>
-    );
-  }
-}
+//Stateless Functional Component (SFC)
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <span className="navbar-brand mb-0 h1">
+        Navbar{" "}
+        <span className="badge badge-pill badge-light">
+          {props.totalCounters}
+        </span>
+      </span>
+    </nav>
+  );
+};
 
 export default NavBar;
